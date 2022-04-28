98e1afefee795b8fe10134e5015c0f3355756547
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-04-28 16:54:07 +0900

    googleでのsignUpのviewを実装 ※logicはエラー出る

---
+++ lib/domain/type_adapter/cart_item/cart_item.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_item.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CartItemAdapter extends TypeAdapter<CartItem> {
+  @override
+  final int typeId = 1;
+
+  @override
+  CartItem read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CartItem(
+      id: fields[0] as String,
+      isNeed: fields[1] as bool,
+      isBought: fields[2] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CartItem obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.isNeed)
+      ..writeByte(2)
+      ..write(obj.isBought);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CartItemAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
