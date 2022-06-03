6d18a354f01e375fa8fca3f0478684d55982b11d
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-06-03 14:44:32 +0900

    不要なimportを削除。

--- lib/view/recipe/recipe_detail/recipe_detail_page.dart
+++ lib/view/recipe/recipe_detail/recipe_detail_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:recipe/state/other_provider/providers.dart';
 import 'package:recipe/components/widgets/recipe_detail_widget/recipe_detail_widget.dart';
