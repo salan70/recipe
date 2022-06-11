e2da883f0d16f5ce371165dfe6e5c8c886c3ada3
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-06-11 11:23:13 +0900

    otherCartItemsのupdate,readのtestを作成。

--- firebase/test/firestore.otherCartItem.test.ts
+++ firebase/test/firestore.otherCartItem.test.ts
@@ -115,41 +115,181 @@ describe('【users/userId/otherCartItems/itemId】', () => {
     })
   })
 
-//   // update
-//   describe('【update】', () => {
-//     test('認証済み, userId同じ, deletedAtあり', async () => {
-//       await testEnv.authenticatedContext(userId).firestore().doc(`deletedUsers/${userId}`).set({
-//         deletedAt: serverTimestamp()
-//       })
-//       await assertFails(
-//         testEnv.authenticatedContext(userId).firestore().doc(`deletedUsers/${userId}`).update({
-//           deletedAt: serverTimestamp()
-//         })
-//       )
-//     })
-//   })
+  // update
+  describe('【update】', () => {
+    describe('【OK', () => {
+        test('認証済み, userId同じ, titleあり(最大文字), subTitleあり(最大文字)', async () => {
+          await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+            createdAt: serverTimestamp(),
+            title: '1',
+            subTitle: '',
+          })
+          await assertSucceeds(
+            testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).update({
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+          )
+        })
+        test('認証済み, userId同じ, titleあり(最小文字), subTitleあり(最小文字)', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+              createdAt: serverTimestamp(),
+              title: '12345',
+              subTitle: 'bbbb',
+            })
+            await assertSucceeds(
+              testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).update({
+                  title: 'a',
+                  subTitle: '',
+              })
+            )
+          })
+      })
+      describe('OK', () => {
+        test('認証済み, userId同じ, titleあり(最大文字), subTitleあり(最大文字)', async () => {
+          await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '1',
+                subTitle: '',
+          })
+          await assertSucceeds(
+            testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).update({
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+          )
+        })
+        test('認証済み, userId同じ, titleあり(最小文字), subTitleあり(最小文字)', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+              createdAt: serverTimestamp(),
+              title: '12345',
+              subTitle: 'bbbb',
+            })
+            await assertSucceeds(
+              testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).update({
+                  title: 'a',
+                  subTitle: '',
+              })
+            )
+          })
+      })
 
-//   // read
-//   describe('【read】', () => {
-//     describe('OK', () => {
-//       test('認証済み, userId同じ, document', async () => {
-//         await testEnv.authenticatedContext(userId).firestore().doc(`deletedUsers/${userId}`).set({
-//           deletedAt: serverTimestamp()
-//         })
-//         await assertFails(
-//           testEnv.authenticatedContext(userId).firestore().doc(`deletedUsers/${userId}`).get()
-//         )
-//       })
-//       test('認証済み, userId同じ, collection', async () => {
-//         await testEnv.authenticatedContext(userId).firestore().doc(`deletedUsers/${userId}`).set({
-//           deletedAt: serverTimestamp()
-//         })
-//         await assertFails(
-//           testEnv.authenticatedContext(userId).firestore().collection(`deletedUsers`).get()
-//         )
-//       })
-//     })
-//   })
+      describe('NG', () => {
+        test('未認証', async () => {
+          await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '1',
+                subTitle: '',
+          })
+          await assertFails(
+            testEnv.unauthenticatedContext().firestore().doc(`users/${userId}/otherCartItems/${itemId}`).update({
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+          )
+        })
+        test('userId違う', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '12345',
+                subTitle: 'bbbb',
+            })
+            await assertFails(
+              testEnv.authenticatedContext(userId).firestore().doc(`users/wrongUser/otherCartItems/${itemId}`).update({
+                  title: 'a',
+                  subTitle: '',
+              })
+            )
+          })
+        test('createdAtあり', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '1',
+                subTitle: '',
+            })
+            await assertFails(
+            testEnv.unauthenticatedContext().firestore().doc(`users/${userId}/otherCartItems/${itemId}`).update({
+                createdAt: serverTimestamp(),
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+                })
+            )
+        })
+        test('titleが21文字', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '1',
+                subTitle: '',
+            })
+            await assertFails(
+                testEnv.unauthenticatedContext().firestore().doc(`users/${userId}/otherCartItems/${itemId}`).update({
+                    createdAt: serverTimestamp(),
+                    title: '123456789012345678901',
+                    subTitle: '12345678901234567890',
+                })
+            )
+        })
+    })
+  })
+
+  // read
+  describe('【read】', () => {
+    describe('OK', () => {
+        test('認証済み, userId同じ, document', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+            await assertSucceeds(
+                testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).get()
+            )
+        })
+        test('認証済み, userId同じ, collection', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+            await assertSucceeds(
+                testEnv.authenticatedContext(userId).firestore().collection(`users/${userId}/otherCartItems`).get()
+            )
+        })
+    })
+
+    describe('NG', () => {
+        test('未認証', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+            await assertFails(
+                testEnv.unauthenticatedContext().firestore().doc(`users/${userId}/otherCartItems/${itemId}`).get()
+            )
+        })
+        test('userId違う, document', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+            await assertFails(
+                testEnv.authenticatedContext(userId).firestore().doc(`users/wrongUser/otherCartItems/${itemId}`).get()
+            )
+        })
+        test('userId違う, collection', async () => {
+            await testEnv.authenticatedContext(userId).firestore().doc(`users/${userId}/otherCartItems/${itemId}`).set({
+                createdAt: serverTimestamp(),
+                title: '12345678901234567890',
+                subTitle: '12345678901234567890',
+            })
+            await assertFails(
+                testEnv.authenticatedContext(userId).firestore().collection(`users/wrongUser/otherCartItems`).get()
+            )
+        })
+    })
+  })
 
   // delete
   describe('【delete】', () => {
