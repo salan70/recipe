58dcffb0c07623036dfffc7f3ceed6584732c69c
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-07-10 12:21:21 +0900

    executeMultiplyのテストを作成

--- test/components/calculation/calculation_test.dart
+++ test/components/calculation/calculation_test.dart
@@ -4,15 +4,32 @@ import 'package:recipe/components/calculation/calculation.dart';
 void main() {
   final calculation = Calculation();
 
-  group('checkNumType test', () {
-    test('null', () {
-      expect(calculation._checkNumType(null), 'null');
-    });
-    test('', () {
-      expect('', 'blank');
-    });
-    test('fraction', () {
-      expect(1 / 2, 'fraction');
+  group('executeMultiply', () {
+    group('int + 〇〇', () {
+      test('null', () {
+        expect(calculation.executeMultiply(1, null), '');
+      });
+      test('', () {
+        expect(calculation.executeMultiply(1, ''), '');
+      });
+      test('int', () {
+        expect(calculation.executeMultiply(1, '1'), '1');
+      });
+      test('double', () {
+        expect(calculation.executeMultiply(1, '1.5'), '1.5');
+      });
+      test('fraction 1', () {
+        expect(calculation.executeMultiply(1, '1/2'), '1/2');
+      });
+      test('fraction 2', () {
+        expect(calculation.executeMultiply(1, '2/2'), '1');
+      });
+      test('mixedFraction 1', () {
+        expect(calculation.executeMultiply(1, '1 1/2'), '1 1/2');
+      });
+      test('mixedFraction 2', () {
+        expect(calculation.executeMultiply(1, '1 2/2'), '2');
+      });
     });
   });
 }
